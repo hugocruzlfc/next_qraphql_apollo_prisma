b836e2fc6b42b1096bdfe7e5503be89058e59e8b
hugo_cruz <hugo.cruz@sivasdescalzo.com>
2023-03-27 11:34:55 +0200

    Add project

---
+++ pages/api/graphql.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+
+import { prisma } from "../../prisma/db";
+import { typeDefs } from "@/graphql/schema";
+import { resolvers } from "@/graphql/resolvers";
+
+export type Context = {
+  prisma: PrismaClient;
+};
+
+const apolloServer = new ApolloServer<Context>({ typeDefs, resolvers });
+
+export default startServerAndCreateNextHandler(apolloServer, {
+  context: async (req, res) => ({ req, res, prisma }),
+});
